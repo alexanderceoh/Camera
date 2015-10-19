7c2502115f28c0f4603aa7e6c1afc40ce2ab05d5
alexanderceoh <alexanderceoh@gmail.com>
2015-10-19 14:15:24 -0400

    play with camera

--- Camera/Camera/ViewController.h
+++ Camera/Camera/ViewController.h
@@ -10,6 +10,22 @@
 
 @interface ViewController : UIViewController
 
+// we are declaring names property to be accessible by any class because it's in .h file
+// swift - var names: NSArray!
+//@property () NSArray * names;
+
+// properties come wtih getter, setter, and a synthesized instance variable
+// getter and setter are methods, the instance variable is a variable
+
+
+
+// the return is on the left side, encased in parenthesis'
+// the minus denotes whether it is an instance method or + which is a class method
+// in the parenthesis is the type, then 'frame' which is the name
+// swift - func createBox(frame: CGRect) -> UIView
+//- (UIView *)createBox:(CGRect)frame;
+
+
 
 @end
 
--- Camera/Camera/ViewController.m
+++ Camera/Camera/ViewController.m
@@ -7,21 +7,114 @@
 //
 
 #import "ViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property UIImagePickerController * pickerController;
+
+@property (weak, nonatomic) IBOutlet UIImageView *mySelfieImageView;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+/*
+
+- (UIView *)createBox:(CGRect)frame {
+    UIView * view = [[UIView alloc] initWithFrame:frame];
+    // swift - let view: UIView = UIView(frame: frame)
+    // allocate is creating an object and puts it in a space in memeory
+    
+    // * denotes a pointer
+    
+    // view2 is pointing to the same spot in memory as view
+    UIView * view2 = view;
+    
+    // now view is pointing to a different spot than view2, because of UIView alloc
+    view = [[UIView alloc] init];
+    
+    
+    // these are making copies because they are not pointers, no *
+    int num1 = 5;
+    
+    int num2 = num1;
+    
+    return view;
+    
+    
+}
+*/
+
+
+
+/* COMES WITH PROPERTY
+
+@synthesize names=_names;
+
+- (void)setNames:(NSArray *)names {
+    
+    _names = names;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSArray *)names {
+    
+    return _names;
+    
+}
+
+COMES WITH PROPERTY */
+
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    
+    if (self != nil) {
+        
+        self.pickerController = [[UIImagePickerController alloc] init];
+        self.pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+//        self.pickerController.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+        self.pickerController.delegate = self;
+        
+        
+    }
+    
+    return self;
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+//    [self.view addSubview:self.pickerController.view];
+    
+    if (self.mySelfieImageView.image == nil) {
+        
+        [self presentViewController:self.pickerController animated:YES completion:nil];
+        
+    }
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    NSString * type = info[UIImagePickerControllerMediaType];
+    
+    if (type == (NSString *)kUTTypeImage) {
+        
+        UIImage * image = info[UIImagePickerControllerOriginalImage];
+        
+        self.mySelfieImageView.image = image;
+        
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    
+    
 }
 
 @end
