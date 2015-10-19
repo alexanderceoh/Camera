b4b3c12f0c3f205c086335f3c8208bbb10c19356
alexanderceoh <alexanderceoh@gmail.com>
2015-10-19 09:25:53 -0400

    initial create

---
+++ Camera/Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by alex oh on 10/19/15.
+//  Copyright © 2015 alex oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
